5f37a402ccb3a2c9f027357be101f96a0dcb19e8
Dileep Kumar <sabbitidileepkumar@gmail.com>
2026-09-08 21:25:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { initStore } from './dataStore'
+import { initSyncListener } from './offlineSync'
+
+initStore()
+initSyncListener()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
